157559497937268a75e7a9bbbeb30903de8864fe
Arthur Flam <arthur.flam@gmail.com>
2013-10-18 16:41:30 +0000

    node ready for prod

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 var fs = require('node-fs');
 var htmlfile = "index.html";
 
-
+app.use(express.compress());
 app.use(express.static(__dirname));
-app.get('/',function(req,res){
-    var html = fs.readFileSync(htmlfile).toString();
-    console.log(req);
-    res.send(html);
-});
+// app.get('/',function(req,res){
+//     var html = fs.readFileSync(htmlfile).toString();
+//     console.log(req);
+//     res.send(html);
+// });
 
 var port = process.env.PORT || 8080;
 app.listen(port,function(){
